5b41f6c591ccc78b4c585b3d0737ff7517f3c1b7
mrcoder57 <tiwariji2300@gmail.com>
2023-11-28 20:53:13 +0530

    create constant

---
+++ src/constants/index.js
@@ -0,0 +1,44 @@
+import rock from "../assets/rock.jpeg"
+import pop from "../assets/pop.jpeg"
+import hiphop from "../assets/hiphop.jpeg"
+import jazz from "../assets/jazz.jpeg"
+import classical from "../assets/classical.jpeg"
+import peaceful from "../assets/peaceful.jpeg"
+import relax from "../assets/relax.jpeg"
+import dance from "../assets/dance.jpeg"
+
+export const musicGenres = [
+    {
+      title: "Rock",
+      bgImage: rock
+    },
+    {
+      title: "Pop",
+      bgImage: pop
+    },
+    {
+      title: "Hip Hop",
+      bgImage: hiphop
+    },
+    {
+      title: "Jazz",
+      bgImage: jazz
+    },
+    {
+      title: "Classical",
+      bgImage: classical
+    },
+    {
+      title: "Peaceful",
+      bgImage: peaceful
+    },
+    {
+      title: "Relax",
+      bgImage: relax
+    },
+    {
+      title: "Dance",
+      bgImage: dance
+    }
+  ];
+  
